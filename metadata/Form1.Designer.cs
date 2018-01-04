585e82bcc8f50393560ac58765c496dfa6471624
Huain <huaing@gmail.com>
2018-01-04 07:56:14 -0500

    Primer Commit

---
+++ metadata/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace metadata
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(482, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(167, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(308, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Carpeta:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Archivo de Salida:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(167, 88);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(308, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(482, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(28, 20);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(41, 160);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Obtener geolocalizacion";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Se extrajo la geolocalizacion a ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(199, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(235, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "fotografias en esta carpeta.";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(41, 206);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(310, 306);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(532, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Formato";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(18, 54);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(74, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Decimales";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(18, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(59, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Grados";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(390, 192);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(656, 320);
+            this.gMapControl1.TabIndex = 12;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 538);
+            this.Controls.Add(this.gMapControl1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Geolocalizacion";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+    }
+}
+
---
+++ metadata/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using ExifLib;
+using GMap.NET.MapProviders;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+
+
+namespace metadata
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            FolderBrowserDialog carpeta = new FolderBrowserDialog();
+
+            DialogResult resultado = carpeta.ShowDialog();
+
+            if (!string.IsNullOrWhiteSpace(carpeta.SelectedPath))
+            {
+                //string[] files = Directory.GetFiles(carpeta.SelectedPath);
+                textBox1.Text = carpeta.SelectedPath;
+
+                //MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog salida = new OpenFileDialog();
+            salida.DefaultExt = "txt";
+            salida.InitialDirectory = textBox1.Text;
+            salida.CheckFileExists = false;
+            if (salida.ShowDialog() == DialogResult.OK)
+                {
+                textBox2.Text = salida.FileName;
+                }
+        }
+        private void posicion_google(List<double> latitud, List<double> longitud)
+        {
+            gMapControl1.Overlays.Clear();
+            gMapControl1.MapProvider = GoogleMapProvider.Instance;
+            //gMapControl1.MapProvider = GoogleSatelliteMapProvider.Instance;
+            //get tiles from server only
+            gMapControl1.Manager.Mode = AccessMode.ServerOnly;
+            //not use proxy
+            GMapProvider.WebProxy = null;
+            gMapControl1.Position = new PointLatLng(latitud[0], longitud[0]);
+            GMapOverlay transparencia = new GMapOverlay("transparencia");
+            for (int i = 0; i < longitud.Count; i++) {
+                GMarkerGoogle marcador = new GMarkerGoogle(new PointLatLng(latitud[i], longitud[i]), GMarkerGoogleType.green);
+                transparencia.Markers.Add(marcador);
+            }
+            //GMarkerGoogle marcador1 = new GMarkerGoogle(new PointLatLng(Convert.ToDouble(textBox1.Text) - .5, Convert.ToDouble(textBox2.Text) - .5), GMarkerGoogleType.green);
+            //GMarkerGoogle marcador2 = new GMarkerGoogle(new PointLatLng(Convert.ToDouble(textBox1.Text) + .5, Convert.ToDouble(textBox2.Text) + .5), GMarkerGoogleType.green);
+            //GMarkerGoogle marcador3 = new GMarkerGoogle(new PointLatLng(Convert.ToDouble(textBox1.Text) + 1, Convert.ToDouble(textBox2.Text) + 1), GMarkerGoogleType.green);
+            //GMarkerGoogle marcador4 = new GMarkerGoogle(new PointLatLng(Convert.ToDouble(textBox1.Text) + 1, Convert.ToDouble(textBox2.Text) - 1.5), GMarkerGoogleType.red);
+            //transparencia.Markers.Add(marcador1);
+            //transparencia.Markers.Add(marcador2);
+            //transparencia.Markers.Add(marcador3);
+            //transparencia.Markers.Add(marcador4);
+            gMapControl1.Overlays.Add(transparencia);
+
+            //zoom min/max; default both = 2
+            //gMapControl1.Refresh();
+            gMapControl1.MinZoom = 1;
+            gMapControl1.MaxZoom = 20;
+            //set zoom
+            gMapControl1.Zoom = 10;
+            gMapControl1.ZoomAndCenterMarkers("transparencia");
+            gMapControl1.CanDragMap = true;
+            gMapControl1.DragButton = MouseButtons.Left;
+
+        }
+        private int extrmetadata(string directorio, string archivodestino)
+        {
+            int cantidad = 0;
+            List<double> latitudes = new List<double>();
+            List<double> longitudes = new List<double>();
+
+            if (!Directory.Exists(directorio))
+            {
+                return 0;
+            }
+            StreamWriter sw = new StreamWriter(archivodestino);
+            richTextBox1.Clear();
+            foreach (string archivo in Directory.GetFiles(directorio, "*.jpg"))
+            {
+
+                double[] latitud;
+                double[] longitud;
+                string latitudref, longitudref;
+                double altitud;
+                double latitudFormato, longitudFormato;
+                DateTime fechacreacion;
+                try
+                {
+                    ExifLib.ExifReader lector = new ExifReader(archivo);
+                    if (lector.GetTagValue<Double[]>(ExifTags.GPSLatitude, out latitud) && lector.GetTagValue<Double[]>(ExifTags.GPSLongitude, out longitud))
+                    {
+                        lector.GetTagValue(ExifTags.DateTime, out fechacreacion);
+                        lector.GetTagValue(ExifTags.GPSLatitudeRef, out latitudref);
+                        lector.GetTagValue(ExifTags.GPSLongitudeRef, out longitudref);
+                        lector.GetTagValue(ExifTags.GPSAltitude, out altitud);
+                        latitudFormato = latitud[0] + latitud[1] / 60 + latitud[2] / 3600;
+                        longitudFormato = longitud[0] + longitud[1] / 60 + longitud[2] / 3600;
+                        
+                        if (latitudref == "S")
+                        {
+                            latitudFormato = latitudFormato * -1;
+                        }
+
+                        if (longitudref == "W")
+                        {
+                            longitudFormato = longitudFormato * -1;
+                        }
+
+                        //Console.WriteLine("{0}\t{10}\t{4} {1}:{2}:{3}\t{8} {5}:{6}:{7}\tAltitud: {9}", Path.GetFileName(archivo), latitud[0], latitud[1], latitud[2], latitudref, longitud[0], longitud[1], longitud[2], longitudref, altitud, fechacreacion);
+                        if (radioButton1.Checked)
+                        {
+                            sw.WriteLine("{0}\t{4} {1}:{2}:{3}\t{8} {5}:{6}:{7}\tAltitud: {9}\t{10}", Path.GetFileName(archivo), latitud[0], latitud[1], latitud[2], latitudref, longitud[0], longitud[1], longitud[2], longitudref, altitud, fechacreacion);
+                            richTextBox1.AppendText(string.Format("{0}\t{4} {1}:{2}:{3}\t{8} {5}:{6}:{7}\tAltitud: {9}\t{10}\r\n", Path.GetFileName(archivo), latitud[0], latitud[1], latitud[2], latitudref, longitud[0], longitud[1], longitud[2], longitudref, altitud, fechacreacion));
+                        }
+                        else
+                        {
+                            sw.WriteLine("{0}\t{1}\t{2}\tAltitud: {3}\t{4}", Path.GetFileName(archivo), latitudFormato, longitudFormato, altitud, fechacreacion);
+                            richTextBox1.AppendText(string.Format("{0}\t{1}\t{2}\tAltitud: {3}\t{4}\r\n", Path.GetFileName(archivo), latitudFormato, longitudFormato, altitud, fechacreacion));
+                        }
+                        cantidad++;
+                        //Console.WriteLine(Path.GetFileName(archivo), latitudFormato, longitudFormato, latitudes.Count);
+                        latitudes.Add(latitudFormato);
+                        longitudes.Add(longitudFormato);
+
+                    }
+                    else { sw.WriteLine("{0} no cuenta con datos de geolocalizacion", Path.GetFileName(archivo)); }
+                }
+                catch
+                {
+                }
+            }
+            posicion_google(latitudes, longitudes);
+            sw.Flush();
+            sw.Close();
+            
+            //Console.ReadKey();
+            return cantidad;
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int cantidad;
+            if (!((textBox1.Text == string.Empty) || (textBox2.Text == string.Empty)))
+            {
+                cantidad = extrmetadata(textBox1.Text, textBox2.Text);
+                label4.Text = Convert.ToString(cantidad);
+
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            //System.Windows.Forms.ToolTip ToolTip1 = new System.Windows.Forms.ToolTip();
+            //ToolTip1.SetToolTip(button1, "Seleccione la carpeta con las fotos cuya geolocalizaion quiere extraer");
+
+        }
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            //System.Windows.Forms.ToolTip ToolTip1 = new System.Windows.Forms.ToolTip();
+            //ToolTip1.SetToolTip(button2, "Seleccione el archivo que guardara la geolocalizacion");
+
+        }
+    }
+}
